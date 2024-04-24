c2d5b7b5ec649fd151c7dec581ba28d8331ddbf2
DESKTOP-5Q6N297\cone_ <carloscastillodeveloper@gmail.com>
2024-04-23 20:09:17 -0600

    commit Inicial

---
+++ src/main/java/com/bankXx/pagosAPI/PagosApiApplication.java
@@ -0,0 +1,13 @@
+package com.bankXx.pagosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankXx/pagosAPI/PagosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankXx.pagosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
